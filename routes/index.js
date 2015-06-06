f33b7235c850866b8d06e08b599758e4bd33ba5d
jesusabad <jesus.abad@gmail.com>
2015-06-06 13:31:12 +0200

    Primera página y favicon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' }); // Cambiamos el de por defecto 'Express' por 'Quiz'
 });
 
 module.exports = router;
