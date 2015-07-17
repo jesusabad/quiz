66797b155f5c0168d07f41d6e16bb52732edfb33
jesusabad <jesus.abad@gmail.com>
2015-07-17 12:45:33 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res) {
 });
 // Autoload de comandos con :quizId
 router.param( 'quizId', quizController.load );  // autoload :quizId
+router.param( 'commentId', commentController.load ); // autoload :commentId
 // Definición de rutas de sesion
 router.get('/login',                       sessionController.new);     // formulario login
 router.post('/login',                      sessionController.create);  // crear sesión
@@ -26,6 +27,8 @@ router.put('/quizes/:quizId(\\d+)',        sessionController.loginRequired, quiz
 router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quizController.destroy);
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+											sessionController.loginRequired, commentController.publish);
 // Añadimos una nueva página para Créditos
 router.get('/author', function(req, res) {
   res.render('author', { autor: 'Jesús Abad Luque', errors: [] });
